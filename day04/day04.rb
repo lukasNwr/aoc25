772a7feaaa28c7e643ce27bc5612a8b082ad7043
Lukáš Novoroľník <lukas.novorolnik@gmail.com>
2025-12-05 09:47:53 +0100

    day4 part2

--- day04/day04.rb
+++ day04/day04.rb
@@ -7,64 +7,59 @@ def draw_map(room)
   end
 end
 
+def in_bounds?(arr, row, col)
+  row >= 0 && row < arr.length && col >= 0 && col < arr[0].length
+end
+
 def check_neighbors(arr, row, col, coords)
-  rolls = 0
-  coords.each do |coord|
-    target_row = row + coord[0]
-    target_col = col + coord[1]
+  # This is returned (ruby returns last expression)
+  # .count counts all elements where the block returns true
+  coords.count do |dr, dc|
+    target_row = row + dr
+    target_col = col + dc
+    in_bounds?(arr, target_row, target_col) && arr[target_row][target_col] == '@'
+  end
+end
 
-    next unless target_row >= 0 && target_row < arr.length && target_col >= 0 && target_col < arr[0].length && (arr.dig(
-      row + coord[0], col + coord[1]
-    ) == '@')
+def num_of_acces_rolls(room, room_copy, coords, part = 1)
+  num_of_accessible_rolls = 0
 
-    rolls += 1
+  room.each_with_index do |row, row_index|
+    row.each_with_index do |col, col_index|
+      neighbors = check_neighbors(room, row_index, col_index, coords)
+
+      next unless neighbors < 4 && col == '@'
+
+      num_of_accessible_rolls += 1
+      room_copy[row_index][col_index] = 'x'
+
+      room[row_index][col_index] = '.' if part == 2
+    end
   end
 
-  rolls
+  num_of_accessible_rolls
 end
 
 if __FILE__ == $0
   room = []
-
   coords = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+  total_accesible_rolls = 0
 
-  count = 0
   lines = File.readlines('input.txt')
   lines.each do |line|
     room.push(line.chomp.chars)
   end
 
-  # Debug: check specific positions
-  puts "Character at [0,0]: '#{room[0][0]}'"
-  puts "Character at [0,1]: '#{room[0][1]}'"
-  puts "Character at [0,2]: '#{room[0][2]}'"
-  puts "First row: #{room[0].join}"
-
-  puts "Second row: #{room[1].join}"
-  puts 'Characters around [0,2]:'
-  puts "  [1,1] = '#{room[1][1]}'"
-  puts "  [1,2] = '#{room[1][2]}'"
-  puts "  [1,3] = '#{room[1][3]}'"
-
-  puts "Room size: #{room.length} x #{room[0].length}"
-
   room_copy = room.map(&:dup)
 
-  room.each_with_index do |row, row_index|
-    row.each_with_index do |_col, col_index|
-      neighbors = check_neighbors(room, row_index, col_index, coords)
+  puts "Part1:  #{num_of_acces_rolls(room, room_copy, coords, 1)}"
 
-      if row_index == 0 && room[row_index][col_index] == '@'
-        puts "Position [#{row_index}, #{col_index}] = '#{room[row_index][col_index]}', neighbors: #{neighbors}"
-      end
-
-      if neighbors < 4 && room[row_index][col_index] == '@'
-        count += 1
-        room_copy[row_index][col_index] = 'x'
-      end
-    end
+  loop do
+    ar = num_of_acces_rolls(room, room_copy, coords, 2)
+    total_accesible_rolls += ar
+    break if ar == 0
   end
-  draw_map(room_copy)
-  puts count
 
+  # draw_map(room_copy)
+  puts "Part2:  #{total_accesible_rolls}"
 end
